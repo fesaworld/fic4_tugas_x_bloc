eeb4f51ac374fbfdbbee413fe3b577350bd6aa8b
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-19 22:45:13 +0700

    add bloc register_event

--- lib/bloc/register/register_event.dart
+++ lib/bloc/register/register_event.dart
@@ -2,3 +2,10 @@ part of 'register_bloc.dart';
 
 @immutable
 abstract class RegisterEvent {}
+
+class SaveRegisterEvent extends RegisterEvent {
+  final RegisterModel request;
+  SaveRegisterEvent({
+    required this.request,
+  });
+}
