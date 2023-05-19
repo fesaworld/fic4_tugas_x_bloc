838245cffe6410619f8c2f24890a51b76cdf08bd
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-19 22:41:20 +0700

    add bloc register_state

--- lib/bloc/register/register_state.dart
+++ lib/bloc/register/register_state.dart
@@ -4,3 +4,13 @@ part of 'register_bloc.dart';
 abstract class RegisterState {}
 
 class RegisterInitial extends RegisterState {}
+
+class RegisterLoading extends RegisterState {}
+
+class RegisterLoaded extends RegisterState {
+  final RegisterResponseModel model;
+
+  RegisterLoaded({
+    required this.model,
+  });
+}
