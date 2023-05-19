8022ab9ff546664c6634c8a3d037cd5f8dc2ef8f
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-19 23:37:01 +0700

    improve ui design register page

--- lib/presentation/pages/register_page.dart
+++ lib/presentation/pages/register_page.dart
@@ -35,62 +35,173 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade400,
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            TextField(
-              decoration: const InputDecoration(labelText: 'Name'),
-              controller: nameController,
+            const Text(
+              'REGISTER',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30),
             ),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Email'),
-              controller: emailController,
+            Divider(color: Colors.black, thickness: 2),
+            SizedBox(
+              height: 20,
             ),
-            TextField(
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-              controller: passwordController,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            BlocConsumer<RegisterBloc, RegisterState>(
-              listener: (context, state) {
-                if (state is RegisterLoaded) {
-                  nameController!.clear();
-                  emailController!.clear();
-                  passwordController!.clear();
-                  //navigasi
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content: Text(
-                            'success register with id: ${state.model.id}')),
-                  );
-                }
-              },
-              builder: (context, state) {
-                if (state is RegisterLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                return ElevatedButton(
-                  onPressed: () {
-                    final requestModel = RegisterModel(
-                      name: nameController!.text,
-                      email: emailController!.text,
-                      password: passwordController!.text,
-                    );
+            Card(
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              elevation: 1,
+              child: Container(
+                height: 400,
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Colors.black, width: 1)),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Nama'),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextFormField(
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              controller: nameController,
+                              onChanged: (name) {},
+                              decoration: InputDecoration(
+                                  hintText: 'Masukkan nama anda',
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                  contentPadding: const EdgeInsets.all(12)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Email'),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              controller: emailController,
+                              onChanged: (email) {},
+                              decoration: InputDecoration(
+                                  hintText: 'Masukkan email anda',
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                  contentPadding: const EdgeInsets.all(12)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Password'),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              controller: passwordController,
+                              onChanged: (password) {},
+                              // obscureText: isObscure,
+                              decoration: InputDecoration(
+                                hintText: 'Masukkan password anda',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                contentPadding: const EdgeInsets.all(10),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      BlocConsumer<RegisterBloc, RegisterState>(
+                        listener: (context, state) {
+                          if (state is RegisterLoaded) {
+                            nameController!.clear();
+                            emailController!.clear();
+                            passwordController!.clear();
+                            //navigasi
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                  content: Text(
+                                      'success register with id: ${state.model.id}')),
+                            );
+                          }
+                        },
+                        builder: (context, state) {
+                          if (state is RegisterLoading) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          return ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey,
+                                side: BorderSide(
+                                    color: Colors.grey.shade700, width: 2)),
+                            onPressed: () {
+                              if (nameController!.text.isNotEmpty &&
+                                  emailController!.text.isNotEmpty &&
+                                  passwordController!.text.isNotEmpty) {
+                                final requestModel = RegisterModel(
+                                  name: nameController!.text,
+                                  email: emailController!.text,
+                                  password: passwordController!.text,
+                                );
 
-                    context
-                        .read<RegisterBloc>()
-                        .add(SaveRegisterEvent(request: requestModel));
-                  },
-                  child: const Text('Register'),
-                );
-              },
+                                context.read<RegisterBloc>().add(
+                                    SaveRegisterEvent(request: requestModel));
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      backgroundColor: Colors.red,
+                                      content:
+                                          Text('Input data in all fields')),
+                                );
+                              }
+                            },
+                            child: const Text('Register'),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
